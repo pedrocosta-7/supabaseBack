de12cd8b996d45b5b49639619635ad18c9e3cf61
Pedro Costa <pedronascimento.cst@gmail.com>
2025-05-21 18:42:19 -0300

    corrigindo

--- app.js
+++ app.js
@@ -32,8 +32,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 const supabase = 
-    supabaseClient.createClient('https://minhaurl', 
-        'minhaAPIKey')
+    supabaseClient.createClient('https://idgijqpidtpuonbrzomf.supabase.co','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlkZ2lqcXBpZHRwdW9uYnJ6b21mIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc3ODcwNjgsImV4cCI6MjA2MzM2MzA2OH0.xOTv42j5QvBv-BS0FT05oY_zwH4pDSxXqnU62feEZU8')
 
 
 app.get('/products', async (req, res) => {
@@ -106,9 +105,9 @@ app.get('/', (req, res) => {
     res.send("Hello I am working my friend Supabase <3");
 });
 
-app.get('*', (req, res) => {
-    res.send("Hello again I am working my friend to the moon and behind <3");
-});
+//app.get('*', (req, res) => {
+//   res.send("Hello again I am working my friend to the moon and behind <3");
+//});
 
 app.listen(3000, () => {
     console.log(`> Ready on http://localhost:3000`);
